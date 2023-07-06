7661953625a5d70c170297efdaaa10f14fb3bd6d
Tanguy Rossel <tanguy.rossel@camptocamp.com>
2023-07-06 16:24:20 +0200

    feat: add Prometheus alerts

--- locals.tf
+++ locals.tf
@@ -4,10 +4,37 @@ locals {
       namespace        = "${var.namespace}"
       snapshotsEnabled = false
       metrics = {
-		enabled = true
+        enabled = true
         serviceMonitor = {
           autodetect = true
           enabled    = true
+        }
+        prometheusRule = {
+          enabled = true
+          spec = [
+            {
+              alert = "VeleroBackupPartialFailures"
+              annotations = {
+                message = "Velero backup {{ $labels.schedule }} has {{ $value | humanizePercentage }} partially failed backups."
+              }
+              expr = "velero_backup_partial_failure_total{schedule!=\"\"} / velero_backup_attempt_total{schedule!=\"\"} > 0.25"
+              for  = "15m"
+              labels = {
+                severity = "warning"
+              }
+            },
+            {
+              alert = "VeleroBackupFailures"
+              annotations = {
+                message = "Velero backup {{ $labels.schedule }} has {{ $value | humanizePercentage }} failed backups."
+              }
+              expr = "velero_backup_partial_failure_total{schedule!=\"\"} / velero_backup_attempt_total{schedule!=\"\"} > 0.25"
+              for  = "15m"
+              labels = {
+                severity = "warning"
+              }
+            }
+          ]
         }
       }
     }
